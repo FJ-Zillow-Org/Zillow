3ca9cd574561266c6eee0464afee57a91fdf2b8f
faithkane3 <faith.x.kane@gmail.com>
2019-11-06 11:59:26 -0600

    Commit updated sql query to limit pull to propertylandusetype 261, single residential

--- acquire.py
+++ acquire.py
@@ -21,97 +21,97 @@ def get_db_url(db, user= env.user, host=env.host, password=env.password):
 
 def get_zillow_data():
     query = '''
-        select 
-        svi.`COUNTY` county,
-        p.`taxamount`/p.`taxvaluedollarcnt` tax_rate,
-        p.`id`,
-        p.`parcelid`,
-        p.`airconditioningtypeid`,
-        act.`airconditioningdesc`,
-        p.`architecturalstyletypeid`,
-        ast.`architecturalstyledesc`,
-        p.`basementsqft`,
-        p.`bathroomcnt`,
-        p.`bedroomcnt`,
-        p.`buildingclasstypeid`,
-        bct.`buildingclassdesc`,
-        p.`buildingqualitytypeid`,
-        p.`calculatedbathnbr`,
-        p.`calculatedfinishedsquarefeet`,
-        p.`decktypeid`,
-        p.`finishedfloor1squarefeet`,
-        p.`finishedsquarefeet12`,
-        p.`finishedsquarefeet13`,
-        p.`finishedsquarefeet15`,
-        p.`finishedsquarefeet50`,
-        p.`finishedsquarefeet6`,
-        p.`fips`,
-        svi.`ST_ABBR` state,
-        p.`fireplacecnt`,
-        p.`fullbathcnt`,
-        p.`garagecarcnt`,
-        p.`garagetotalsqft`,
-        p.`hashottuborspa`,
-        p.`heatingorsystemtypeid`,
-        hst.`heatingorsystemdesc`,
-        p.`latitude`,
-        p.`longitude`,
-        p.`lotsizesquarefeet`,
-        p.`poolcnt`,
-        p.`poolsizesum`,
-        p.`pooltypeid10`,
-        p.`pooltypeid2`,
-        p.`pooltypeid7`,
-        p.`propertycountylandusecode`,
-        p.`propertylandusetypeid`,
-        plut.`propertylandusedesc`,
-        p.`propertyzoningdesc`,
-        p.`rawcensustractandblock`,
-        p.`regionidcity`,
-        p.`regionidcounty`,
-        p.`regionidneighborhood`,
-        p.`regionidzip`,
-        p.`roomcnt`,
-        p.`storytypeid`,
-        st.`storydesc`,
-        p.`taxvaluedollarcnt`,
-        p.`threequarterbathnbr`,
-        p.`unitcnt`,
-        p.`yardbuildingsqft17`,
-        p.`yardbuildingsqft26`,
-        p.`yearbuilt`,
-        p.`numberofstories`,
-        p.`fireplaceflag`,
-        p.`structuretaxvaluedollarcnt`,
-        p.`assessmentyear`,
-        p.`landtaxvaluedollarcnt`,
-        p.`taxamount`,
-        p.`taxdelinquencyflag`,
-        p.`taxdelinquencyyear`, 
-        p.`typeconstructiontypeid`,
-        tct.`typeconstructiondesc`,
-        p.`censustractandblock`,
-        pred.`transactiondate`,
-        pred.`logerror`,
-        m.`transactions`
+    select 
+    svi.`COUNTY` county,
+    p.`taxamount`/p.`taxvaluedollarcnt` tax_rate,
+    p.`id`,
+    p.`parcelid`,
+    p.`airconditioningtypeid`,
+    act.`airconditioningdesc`,
+	p.`architecturalstyletypeid`,
+	ast.`architecturalstyledesc`,
+    p.`basementsqft`,
+	p.`bathroomcnt`,
+    p.`bedroomcnt`,
+	p.`buildingclasstypeid`,
+	bct.`buildingclassdesc`,
+    p.`buildingqualitytypeid`,
+    p.`calculatedbathnbr`,
+    p.`calculatedfinishedsquarefeet`,
+    p.`decktypeid`,
+    p.`finishedfloor1squarefeet`,
+    p.`finishedsquarefeet12`,
+    p.`finishedsquarefeet13`,
+    p.`finishedsquarefeet15`,
+    p.`finishedsquarefeet50`,
+    p.`finishedsquarefeet6`,
+    p.`fips`,
+    svi.`ST_ABBR` state,
+    p.`fireplacecnt`,
+    p.`fullbathcnt`,
+    p.`garagecarcnt`,
+    p.`garagetotalsqft`,
+    p.`hashottuborspa`,
+    p.`heatingorsystemtypeid`,
+    hst.`heatingorsystemdesc`,
+    p.`latitude`,
+    p.`longitude`,
+    p.`lotsizesquarefeet`,
+    p.`poolcnt`,
+    p.`poolsizesum`,
+    p.`pooltypeid10`,
+    p.`pooltypeid2`,
+    p.`pooltypeid7`,
+    p.`propertycountylandusecode`,
+    p.`propertylandusetypeid`,
+    plut.`propertylandusedesc`,
+    p.`propertyzoningdesc`,
+    p.`rawcensustractandblock`,
+    p.`regionidcity`,
+    p.`regionidcounty`,
+    p.`regionidneighborhood`,
+    p.`regionidzip`,
+    p.`roomcnt`,
+	p.`storytypeid`,
+	st.`storydesc`,
+    p.`taxvaluedollarcnt`,
+    p.`threequarterbathnbr`,
+    p.`unitcnt`,
+    p.`yardbuildingsqft17`,
+    p.`yardbuildingsqft26`,
+    p.`yearbuilt`,
+    p.`numberofstories`,
+    p.`fireplaceflag`,
+    p.`structuretaxvaluedollarcnt`,
+    p.`assessmentyear`,
+    p.`landtaxvaluedollarcnt`,
+    p.`taxamount`,
+    p.`taxdelinquencyflag`,
+    p.`taxdelinquencyyear`, 
+	p.`typeconstructiontypeid`,
+	tct.`typeconstructiondesc`,
+    p.`censustractandblock`,
+    pred.`transactiondate`,
+    pred.`logerror`,
+    m.`transactions`
     from 
-        `properties_2017` p
+	`properties_2017` p
     inner join `predictions_2017`  pred
-        on p.`parcelid` = pred.`parcelid` 
+    on p.`parcelid` = pred.`parcelid` 
     inner join 
-        (select 
-            `parcelid`, 
-            max(`transactiondate`) `lasttransactiondate`, 
-            max(`id`) `maxid`, 
-            count(*) `transactions`
-        from 
-            predictions_2017
-        group by 
-            `parcelid`
-        ) m
-        on 
-        pred.parcelid = m.parcelid
-        and pred.transactiondate = m.lasttransactiondate
+	(select 
+		`parcelid`, 
+		max(`transactiondate`) `lasttransactiondate`, 
+		max(`id`) `maxid`, 
+		count(*) `transactions`
+	from 
+		predictions_2017
+	group by 
+		`parcelid`
+	) m
+	on 
+	pred.parcelid = m.parcelid
+	and pred.transactiondate = m.lasttransactiondate
     left join `propertylandusetype` plut
         on p.`propertylandusetypeid` = plut.`propertylandusetypeid`
             
@@ -131,8 +131,10 @@ def get_zillow_data():
         using(`typeconstructiontypeid`)
     where 
         p.`latitude` is not null
-        and p.`longitude` is not null;
-        '''
+        and p.`longitude` is not null
+        and p.bedroomcnt > 0 and p.bathroomcnt > 0
+        and plut.propertylandusetypeid = '261';
+    '''
 
     df = pd.read_sql(query, get_db_url('zillow'))
     return df
